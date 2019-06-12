7ab5a2840479371a23cce66513e0d4df507f2c0d
C3NZ <cenz@cenz.io>
2019-06-12 10:13:42 -0700

    [feat] complete linked list implementation

--- Code/linkedlist.py
+++ Code/linkedlist.py
@@ -1,7 +1,7 @@
 #!python
 
-class Node(object):
 
+class Node(object):
     def __init__(self, data):
         """Initialize this node with the given data."""
         self.data = data
@@ -9,11 +9,10 @@ class Node(object):
 
     def __repr__(self):
         """Return a string representation of this node."""
-        return 'Node({!r})'.format(self.data)
+        return "Node({!r})".format(self.data)
 
 
 class LinkedList(object):
-
     def __init__(self, iterable=None):
         """Initialize this linked list and append the given items, if any."""
         self.head = None  # First node
@@ -26,17 +25,19 @@ class LinkedList(object):
 
     def __str__(self):
         """Return a formatted string representation of this linked list."""
-        items = ['({!r})'.format(item) for item in self.items()]
-        return '[{}]'.format(' -> '.join(items))
+        items = ["({!r})".format(item) for item in self.items()]
+        return "[{}]".format(" -> ".join(items))
 
     def __repr__(self):
         """Return a string representation of this linked list."""
-        return 'LinkedList({!r})'.format(self.items())
+        return "LinkedList({!r})".format(self.items())
 
     def items(self):
-        """Return a list of all items in this linked list.
-        Best and worst case running time: Theta(n) for n items in the list
-        because we always need to loop through all n nodes."""
+        """
+            Return a list of all items in this linked list.
+            Best and worst case running time: Theta(n) for n items in the list
+            because we always need to loop through all n nodes.
+        """
         # Create an empty list of results
         result = []  # Constant time to create a new list
         # Start at the head node
@@ -55,8 +56,12 @@ class LinkedList(object):
         return self.head is None
 
     def length(self):
-        """Return the length of this linked list by traversing its nodes.
-        Best and worst case running time: ??? under what conditions? [TODO]"""
+        """
+            Return the length of this linked list by traversing its nodes.
+            Best and worst case running time: O(n) for worst and best case.
+            This is because you have to iterate the entire linked list of n items
+            until you've counted all of them.
+        """
         # Node counter initialized to zero
         node_count = 0
         # Start at the head node
@@ -71,28 +76,70 @@ class LinkedList(object):
         return node_count
 
     def get_at_index(self, index):
-        """Return the item at the given index in this linked list, or
-        raise ValueError if the given index is out of range of the list size.
-        Best case running time: ??? under what conditions? [TODO]
-        Worst case running time: ??? under what conditions? [TODO]"""
+        """
+            Return the item at the given index in this linked list, or
+            raise ValueError if the given index is out of range of the list size.
+            Best case running time: Omega(1) - Where we're getting the item at the first index.
+            Worst case running time: O(n) - Where we're getting the item at the nth index.
+        """
         # Check if the given index is out of range and if so raise an error
         if not (0 <= index < self.size):
-            raise ValueError('List index out of range: {}'.format(index))
-        # TODO: Find the node at the given index and return its data
+            raise ValueError("List index out of range: {}".format(index))
+
+        curr_node = self.head
+        for i in range(index):
+            curr_node = curr_node.next
+
+        return curr_node.data
 
     def insert_at_index(self, index, item):
-        """Insert the given item at the given index in this linked list, or
-        raise ValueError if the given index is out of range of the list size.
-        Best case running time: ??? under what conditions? [TODO]
-        Worst case running time: ??? under what conditions? [TODO]"""
+        """
+            Insert the given item at the given index in this linked list, or
+            raise ValueError if the given index is out of range of the list size.
+            Best case running time: Omega(1) - Where we're inserting at either the head or the tail.
+            Worst case running time: Theta(n) - Where we're inserting at the n-1 index.
+        """
         # Check if the given index is out of range and if so raise an error
         if not (0 <= index <= self.size):
-            raise ValueError('List index out of range: {}'.format(index))
-        # TODO: Find the node before the given index and insert item after it
+            raise ValueError("List index out of range: {}".format(index))
+
+        # Check if the linked list is empty
+        if self.is_empty():
+            self.append(item)
+            return
+
+        # Prepend if we're inserting at the head
+        if index == 0:
+            self.prepend(item)
+            return
+
+        # Append if we're inserting at the tail
+        if index == self.size:
+            self.append(item)
+            return
+
+        curr_node = self.head
+
+        for _ in range(index - 1):
+            curr_node = curr_node.next
+
+        new_node = Node(item)
+        # Grab what the current node points to
+        temp_node = curr_node.next
+
+        # Perform swaps to point the current node to our new node
+        # and our new node to the current nodes prior next.
+        curr_node.next = new_node
+        new_node.next = temp_node
+        self.size += 1
 
     def append(self, item):
-        """Insert the given item at the tail of this linked list.
-        Best and worst case running time: ??? under what conditions? [TODO]"""
+        """
+            Insert the given item at the tail of this linked list.
+            Best and worst case running time: Omega(1) - Because for however large
+            are linkedlist is we're always performing a constant amount of
+            operations
+        """
         # Create a new node to hold the given item
         new_node = Node(item)
         # Check if this linked list is empty
@@ -104,10 +151,14 @@ class LinkedList(object):
             self.tail.next = new_node
         # Update tail to new node regardless
         self.tail = new_node
+        self.size += 1
 
     def prepend(self, item):
-        """Insert the given item at the head of this linked list.
-        Best and worst case running time: ??? under what conditions? [TODO]"""
+        """
+            Insert the given item at the head of this linked list.
+            Best and worst case running time: O(1) - Constant amount of operations
+            regardless of the size of the linked list
+        """
         # Create a new node to hold the given item
         new_node = Node(item)
         # Check if this linked list is empty
@@ -119,12 +170,15 @@ class LinkedList(object):
             new_node.next = self.head
         # Update head to new node regardless
         self.head = new_node
+        self.size += 1
 
     def find(self, quality):
-        """Return an item from this linked list satisfying the given quality.
-        Best case running time: Omega(1) if item is near the head of the list.
-        Worst case running time: O(n) if item is near the tail of the list or
-        not present and we need to loop through all n nodes in the list."""
+        """
+            Return an item from this linked list satisfying the given quality.
+            Best case running time: Omega(1) if item is near the head of the list.
+            Worst case running time: O(n) if item is near the tail of the list or
+            not present and we need to loop through all n nodes in the list.
+        """
         # Start at the head node
         node = self.head  # Constant time to assign a variable reference
         # Loop until the node is None, which is one node too far past the tail
@@ -145,12 +199,25 @@ class LinkedList(object):
         Worst case running time: ??? under what conditions? [TODO]"""
         # TODO: Find the node containing the given old_item and replace its
         # data with new_item, without creating a new node object
-        pass
+
+        if self.is_empty():
+            raise ValueError("Cannot replace an item within an empty linked list.")
+
+        curr_node = self.head
+        for _ in range(self.size):
+            if curr_node.data == old_item:
+                curr_node.data = new_item
+                return
+            curr_node = curr_node.next
+
+        raise ValueError("The old item could not be found within the list.")
 
     def delete(self, item):
-        """Delete the given item from this linked list, or raise ValueError.
-        Best case running time: ??? under what conditions? [TODO]
-        Worst case running time: ??? under what conditions? [TODO]"""
+        """
+            Delete the given item from this linked list, or raise ValueError.
+            Best case running time: Omega(1) - Where the item to delete is the first item in the ll
+            Worst case running time: O(n) - Where the item to delete is the last item in the list.
+        """
         # Start at the head node
         node = self.head
         # Keep track of the node before the one containing the given item
@@ -189,44 +256,45 @@ class LinkedList(object):
                     previous.next = None
                 # Update tail to the previous node regardless
                 self.tail = previous
+            self.size -= 1
         else:
             # Otherwise raise an error to tell the user that delete has failed
-            raise ValueError('Item not found: {}'.format(item))
+            raise ValueError("Item not found: {}".format(item))
 
 
 def test_linked_list():
     ll = LinkedList()
     print(ll)
 
-    print('Appending items:')
-    ll.append('A')
+    print("Appending items:")
+    ll.append("A")
     print(ll)
-    ll.append('B')
+    ll.append("B")
     print(ll)
-    ll.append('C')
+    ll.append("C")
     print(ll)
-    print('head: {}'.format(ll.head))
-    print('tail: {}'.format(ll.tail))
-    print('size: {}'.format(ll.size))
-    print('length: {}'.format(ll.length()))
+    print("head: {}".format(ll.head))
+    print("tail: {}".format(ll.tail))
+    print("size: {}".format(ll.size))
+    print("length: {}".format(ll.length()))
 
-    print('Getting items by index:')
+    print("Getting items by index:")
     for index in range(ll.size):
         item = ll.get_at_index(index)
-        print('get_at_index({}): {!r}'.format(index, item))
+        print("get_at_index({}): {!r}".format(index, item))
 
-    print('Deleting items:')
-    ll.delete('B')
+    print("Deleting items:")
+    ll.delete("B")
     print(ll)
-    ll.delete('C')
+    ll.delete("C")
     print(ll)
-    ll.delete('A')
+    ll.delete("A")
     print(ll)
-    print('head: {}'.format(ll.head))
-    print('tail: {}'.format(ll.tail))
-    print('size: {}'.format(ll.size))
-    print('length: {}'.format(ll.length()))
+    print("head: {}".format(ll.head))
+    print("tail: {}".format(ll.tail))
+    print("size: {}".format(ll.size))
+    print("length: {}".format(ll.length()))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_linked_list()
