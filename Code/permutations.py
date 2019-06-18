da203f60b5115df9b2fdef3add33f4fa3bc587ae
C3NZ <cenz@cenz.io>
2019-06-17 21:10:46 -0700

    [etc] document permutation and anagram function

--- Code/permutations.py
+++ Code/permutations.py
@@ -7,10 +7,38 @@ from recursion import fast_permutation
 STR_LIST = "abcdefghijklmnopqrstuvwxyz"
 
 
-def generate_permutation(
-    iterable: list = [], num_items: int = 3, use_letters: bool = False
-):
-    if not iterable:
+def create_word_set() -> set:
+    """
+        Generate a set of words from a unix based word file
+    """
+    word_list = []
+    with open("/usr/share/dict/words", "r") as word_file:
+        for line in word_file:
+            word_list.append(line.strip())
+
+    return set(word_list)
+
+
+WORD_SET = create_word_set()
+
+
+def generate_permutations(
+    char_list: list = [], num_items: int = 3, use_letters: bool = False
+) -> list:
+    """
+        Generate permutations based on the input specified by the user
+
+        Arguments:
+            char_list - The list of characters to permutate
+            num_items - If char_list isn't specified, will be used for
+                        generating a list of num_items chars
+            use_letters - specifies if we should use characters for
+                          generating characters
+
+        Returns:
+            A list of all permutations our algorithm generated
+    """
+    if not char_list:
         if use_letters:
             orig_list = [STR_LIST[i] for i in range(num_items)]
             all_perms = fast_permutation(orig_list, num_items)
@@ -20,9 +48,32 @@ def generate_permutation(
 
         return all_perms
     else:
-        all_perms = fast_permutation(iterable, len(iterable))
+        all_perms = fast_permutation(char_list, len(char_list))
 
         return all_perms
 
 
-print(generate_permutation(num_items=5))
+def generate_anagram(char_list: list = []):
+    """
+        Generate algorithms based on our permutation generating function
+
+        Arguments:
+            char_list - The list of letters we're trying to generate anagrams of
+        Returns:
+            A list of all the anagrams that can be made given an input
+    """
+    all_perms = generate_permutations(char_list)
+    input_word = "".join(char_list)
+    anagrams = []
+
+    # Iterate through all the permutations and see if any permutation
+    # generated an anagram
+    for perm in all_perms:
+        if not perm == input_word and perm in WORD_SET:
+            anagrams.append(perm)
+
+    return anagrams
+
+
+print(generate_permutations(num_items=5))
+print(generate_anagram(list("aeprs")))
