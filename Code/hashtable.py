cde0370090d540881c6e379722011e9d095bc5d5
C3NZ <cenz@cenz.io>
2019-06-19 14:55:59 -0700

    [feat] implement all of the hash tables functions

--- Code/hashtable.py
+++ Code/hashtable.py
@@ -4,7 +4,6 @@ from linkedlist import LinkedList
 
 
 class HashTable(object):
-
     def __init__(self, init_size=8):
         """Initialize this hash table with the given initial size."""
         self.buckets = [LinkedList() for i in range(init_size)]
@@ -12,12 +11,12 @@ class HashTable(object):
 
     def __str__(self):
         """Return a formatted string representation of this hash table."""
-        items = ['{!r}: {!r}'.format(key, val) for key, val in self.items()]
-        return '{' + ', '.join(items) + '}'
+        items = ["{!r}: {!r}".format(key, val) for key, val in self.items()]
+        return "{" + ", ".join(items) + "}"
 
     def __repr__(self):
         """Return a string representation of this hash table."""
-        return 'HashTable({!r})'.format(self.items())
+        return "HashTable({!r})".format(self.items())
 
     def _bucket_index(self, key):
         """Return the bucket index where the given key would be stored."""
@@ -27,7 +26,7 @@ class HashTable(object):
         """Return the load factor, the ratio of number of entries to buckets.
         Best and worst case running time: ??? under what conditions? [TODO]"""
         # TODO: Calculate load factor
-        # return ...
+        return self.size / len(self.buckets)
 
     def keys(self):
         """Return a list of all keys in this hash table.
@@ -95,7 +94,7 @@ class HashTable(object):
             assert len(entry) == 2
             return entry[1]
         else:  # Not found
-            raise KeyError('Key not found: {}'.format(key))
+            raise KeyError("Key not found: {}".format(key))
 
     def set(self, key, value):
         """Insert or update the given key with its associated value.
@@ -111,12 +110,14 @@ class HashTable(object):
             # In this case, the given key's value is being updated
             # Remove the old key-value entry from the bucket first
             bucket.delete(entry)
+            self.size -= 1
         # Insert the new key-value entry into the bucket in either case
         bucket.append((key, value))
-        # TODO: Check if the load factor exceeds a threshold such as 0.75
-        # ...
-        # TODO: If so, automatically resize to reduce the load factor
-        # ...
+        self.size += 1
+
+        if self.load_factor() > 0.75:
+            doubled_size = 2 * len(self.buckets)
+            self._resize(new_size=doubled_size)
 
     def delete(self, key):
         """Delete the given key and its associated value, or raise KeyError.
@@ -130,8 +131,9 @@ class HashTable(object):
         if entry is not None:  # Found
             # Remove the key-value entry from the bucket
             bucket.delete(entry)
+            self.size -= 1
         else:  # Not found
-            raise KeyError('Key not found: {}'.format(key))
+            raise KeyError("Key not found: {}".format(key))
 
     def _resize(self, new_size=None):
         """Resize this hash table's buckets and rehash all key-value entries.
@@ -145,60 +147,59 @@ class HashTable(object):
         # Option to reduce size if buckets are sparsely filled (low load factor)
         elif new_size is 0:
             new_size = len(self.buckets) / 2  # Half size
-        # TODO: Get a list to temporarily hold all current key-value entries
-        # ...
-        # TODO: Create a new list of new_size total empty linked list buckets
-        # ...
-        # TODO: Insert each key-value entry into the new list of buckets,
-        # which will rehash them into a new bucket index based on the new size
-        # ...
+
+        all_items = self.items()
+        self.buckets = [LinkedList() for _ in range(new_size)]
+        self.size = 0
+        for key, value in all_items:
+            self.set(key, value)
 
 
 def test_hash_table():
     ht = HashTable(4)
-    print('HashTable: ' + str(ht))
+    print("HashTable: " + str(ht))
 
-    print('Setting entries:')
-    ht.set('I', 1)
-    print('set(I, 1): ' + str(ht))
-    ht.set('V', 5)
-    print('set(V, 5): ' + str(ht))
-    print('size: ' + str(ht.size))
-    print('length: ' + str(ht.length()))
-    print('buckets: ' + str(len(ht.buckets)))
-    print('load_factor: ' + str(ht.load_factor()))
-    ht.set('X', 10)
-    print('set(X, 10): ' + str(ht))
-    ht.set('L', 50)  # Should trigger resize
-    print('set(L, 50): ' + str(ht))
-    print('size: ' + str(ht.size))
-    print('length: ' + str(ht.length()))
-    print('buckets: ' + str(len(ht.buckets)))
-    print('load_factor: ' + str(ht.load_factor()))
+    print("Setting entries:")
+    ht.set("I", 1)
+    print("set(I, 1): " + str(ht))
+    ht.set("V", 5)
+    print("set(V, 5): " + str(ht))
+    print("size: " + str(ht.size))
+    print("length: " + str(ht.length()))
+    print("buckets: " + str(len(ht.buckets)))
+    print("load_factor: " + str(ht.load_factor()))
+    ht.set("X", 10)
+    print("set(X, 10): " + str(ht))
+    ht.set("L", 50)  # Should trigger resize
+    print("set(L, 50): " + str(ht))
+    print("size: " + str(ht.size))
+    print("length: " + str(ht.length()))
+    print("buckets: " + str(len(ht.buckets)))
+    print("load_factor: " + str(ht.load_factor()))
 
-    print('Getting entries:')
-    print('get(I): ' + str(ht.get('I')))
-    print('get(V): ' + str(ht.get('V')))
-    print('get(X): ' + str(ht.get('X')))
-    print('get(L): ' + str(ht.get('L')))
-    print('contains(X): ' + str(ht.contains('X')))
-    print('contains(Z): ' + str(ht.contains('Z')))
+    print("Getting entries:")
+    print("get(I): " + str(ht.get("I")))
+    print("get(V): " + str(ht.get("V")))
+    print("get(X): " + str(ht.get("X")))
+    print("get(L): " + str(ht.get("L")))
+    print("contains(X): " + str(ht.contains("X")))
+    print("contains(Z): " + str(ht.contains("Z")))
 
-    print('Deleting entries:')
-    ht.delete('I')
-    print('delete(I): ' + str(ht))
-    ht.delete('V')
-    print('delete(V): ' + str(ht))
-    ht.delete('X')
-    print('delete(X): ' + str(ht))
-    ht.delete('L')
-    print('delete(L): ' + str(ht))
-    print('contains(X): ' + str(ht.contains('X')))
-    print('size: ' + str(ht.size))
-    print('length: ' + str(ht.length()))
-    print('buckets: ' + str(len(ht.buckets)))
-    print('load_factor: ' + str(ht.load_factor()))
+    print("Deleting entries:")
+    ht.delete("I")
+    print("delete(I): " + str(ht))
+    ht.delete("V")
+    print("delete(V): " + str(ht))
+    ht.delete("X")
+    print("delete(X): " + str(ht))
+    ht.delete("L")
+    print("delete(L): " + str(ht))
+    print("contains(X): " + str(ht.contains("X")))
+    print("size: " + str(ht.size))
+    print("length: " + str(ht.length()))
+    print("buckets: " + str(len(ht.buckets)))
+    print("load_factor: " + str(ht.load_factor()))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_hash_table()
