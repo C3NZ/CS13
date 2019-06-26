2e864a66a25a546bd68e98736e1b8c0007adb653
C3NZ <cenz@cenz.io>
2019-06-26 15:31:36 -0700

    [feat] add tests for popping from the front

--- Code/deque_test.py
+++ Code/deque_test.py
@@ -18,15 +18,55 @@ class DequeTest(unittest.TestCase):
 
         deque.push_front("a")
         assert deque.size == 1
-        assert deque.front.data == "a"
-        assert deque.back.data == "a"
+        assert deque.front() == "a"
+        assert deque.back() == "a"
 
         deque.push_front("b")
         assert deque.size == 2
-        assert deque.front.data == "b"
-        assert deque.back.data == "a"
+        assert deque.front() == "b"
+        assert deque.back() == "a"
 
         deque.push_front("d")
         assert deque.size == 3
-        assert deque.front.data == "d"
-        assert deque.back.data == "a"
+        assert deque.front() == "d"
+        assert deque.back() == "a"
+
+    def test_push_back(self):
+        deque = Deque()
+
+        deque.push_back("a")
+        assert deque.size == 1
+        assert deque.front() == "a"
+        assert deque.back() == "a"
+
+        deque.push_back("b")
+        assert deque.size == 2
+        assert deque.front() == "a"
+        assert deque.back() == "b"
+
+        deque.push_back("c")
+        assert deque.size == 3
+
+        assert deque.front() == "a"
+        assert deque.back() == "c"
+
+    def test_pop_front(self):
+        deque = Deque(["a", "b", "c"])
+        assert deque.size == 3
+        assert deque.front() == "a"
+        assert deque.back() == "c"
+
+        assert deque.pop_front() == "a"
+        assert deque.size == 2
+        assert deque.front() == "b"
+        assert deque.back() == "c"
+
+        assert deque.pop_front() == "b"
+        assert deque.size == 1
+        assert deque.front() == "c"
+        assert deque.back() == "c"
+
+        assert deque.pop_front() == "c"
+        assert deque.size == 0
+        assert deque.front() == None
+        assert deque.back() == None
